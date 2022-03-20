b099e5dcacdbac6755df8dd5634a5ea0be7282ca
promisingdev72 <promising.dev72@gmail.com>
2022-03-20 12:44:20 +1000

    update

--- Front/src/Components/Header.js
+++ Front/src/Components/Header.js
@@ -2,45 +2,54 @@ import React from 'react';
 import logoImage from '../assets/logo/logo_white.svg';
 import langIcon from '../assets/images/langIcon.svg'
 import { Link, useLocation } from "react-router-dom";
+import { library, icon } from '@fortawesome/fontawesome-svg-core'
+import { faCamera } from '@fortawesome/free-solid-svg-icons'
 import '../assets/css/style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
 
-    const location = useLocation();
+    library.add(faCamera)
+    const camera = icon({ prefix: 'fas', iconName: 'camera' })
+    // const location = useLocation();
+    console.log(camera);
 
     const showMenu = () =>{
         
     }
 
-    console.log(location.pathname);
+    // console.log(location.pathname);
 
     return (
         <>
             <div className='position-relative'>
-                <nav style={{ height: '20px !important' }} className={location.pathname == '/'?'navbar navbar-expand-lg  w-100 position-fixed':'navbar navbar-expand-lg w-100 navbar-back-img position-fixed'}>
-                    <img src={logoImage} width={100} height={70}></img>
-                    <button className="navbar-toggler" onClick={showMenu} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                <nav className='navbar navbar-expand-lg w-100 navbar-back-color'>
+                    <img src={logoImage} width={140} height={100}></img>
+                    <button className="bdcolor-yellow navbar-toggler" onClick={showMenu} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon d-flex justify-content-center align-items-center color-yellow fs-25">
+                            <FontAwesomeIcon icon={faBars} />
+                        </span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item active">
-                                <Link to="/" className='nav-link text-white fs-14'>
+                                <Link to="/" className='nav-link text-white fs-20'>
                                     <div className='position-relative forUnderline'>Sell Car</div>
                                 </Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/about" className='nav-link text-white fs-14'>
+                                <Link to="/about" className='nav-link text-white fs-20'>
                                     <div className='position-relative forUnderline'>About Us</div>
                                 </Link>
                             </li>
                             <li className="nav-item">
-                                <Link to="/contact" className='nav-link text-white fs-14'>
+                                <Link to="/contact" className='nav-link text-white fs-20'>
                                     <div className='position-relative forUnderline'>Contact Us</div>
                                 </Link>
                             </li>
                             <li className="nav-item">
-                                <span  className='nav-link text-white fs-14 cusor-pointer'>EN
+                                <span  className='nav-link text-white fs-20 cusor-pointer'>EN
                                     <img src={langIcon} width={10} height={5}></img>
                                 </span>
                             </li>
