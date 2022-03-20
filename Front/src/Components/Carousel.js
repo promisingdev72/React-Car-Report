438a21018933c29f9cdca69c780650ec0fa340ac
promisingdev72 <promising.dev72@gmail.com>
2022-03-21 00:34:36 +1000

    update

--- Front/src/Components/Carousel.js
+++ Front/src/Components/Carousel.js
@@ -8,63 +8,109 @@ const Carousel = () =>{
         infinite: true,
         speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        arrows:false,
+        initialSlide: 0,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     };
     
     return(
         <>
             <div>
                 <Slider {...settings}>
-                    <div>
-                        <h3>LEANDER</h3>
-                        <h4>"Bought the full report and taken to the dealer who wanted to sell me my new Seat. 
-                            After negotiating hard on the basis of the real sales price in the report, 
-                            we were able to negotiate €250. "
-                        </h4>
+                    <div className="carouselItem">
+                        <div className="item">
+                            <h5>LEANDER</h5>
+                            <h6>"Bought the full report and taken to the dealer who wanted to sell me my new Seat. 
+                                After negotiating hard on the basis of the real sales price in the report, 
+                                we were able to negotiate €250. "
+                            </h6>
+                        </div>
                     </div>
-                    <div>
-                        <h3>NIENKE</h3>
-                        <h4>" Receive a clear and comprehensive report. 
-                            It has all the information I wanted to know before selling my car. Top! "
-                        </h4>
+                    <div className="carouselItem">
+                        <div className="item">
+                            <h5>NIENKE</h5>
+                            <h6>" Receive a clear and comprehensive report. 
+                                It has all the information I wanted to know before selling my car. Top! "
+                            </h6>
+                        </div>
                     </div>
-                    <div>
-                        <h3>KAJI</h3>
-                        <h4>" Finally a transparent report "</h4>
+                    <div className="carouselItem">
+                        <div className="item">
+                            <h5>KAJI</h5>
+                            <h6>" Finally a transparent report "</h6>
+                        </div>
                     </div>
-                    <div>
-                        <h3>LEANDER</h3>
-                        <h4>"Bought the full report and taken to the dealer who wanted to sell me my new Seat. 
-                            After negotiating hard on the basis of the real sales price in the report, 
-                            we were able to negotiate €250. "
-                        </h4>
+                    <div className="carouselItem">
+                        <div className="item">
+                            <h5>LEANDER</h5>
+                            <h6>"Bought the full report and taken to the dealer who wanted to sell me my new Seat. 
+                                After negotiating hard on the basis of the real sales price in the report, 
+                                we were able to negotiate €250. "
+                            </h6>
+                        </div>
                     </div>
-                    <div>
-                        <h3>NIENKE</h3>
-                        <h4>" Receive a clear and comprehensive report. 
-                            It has all the information I wanted to know before selling my car. Top! "
-                        </h4>
+                    <div className="carouselItem">
+                        <div className="item">
+                            <h5>NIENKE</h5>
+                            <h6>" Receive a clear and comprehensive report. 
+                                It has all the information I wanted to know before selling my car. Top! "
+                            </h6>
+                        </div>
                     </div>
-                    <div>
-                        <h3>KAJI</h3>
-                        <h4>" Finally a transparent report "</h4>
+                    <div className="carouselItem">
+                        <div className="item">
+                            <h5>KAJI</h5>
+                            <h6>" Finally a transparent report "</h6>
+                        </div>
                     </div>
-                    <div>
-                        <h3>LEANDER</h3>
-                        <h4>"Bought the full report and taken to the dealer who wanted to sell me my new Seat. 
-                            After negotiating hard on the basis of the real sales price in the report, 
-                            we were able to negotiate €250. "
-                        </h4>
+                    <div className="carouselItem">
+                        <div className="item">
+                            <h5>LEANDER</h5>
+                            <h6>"Bought the full report and taken to the dealer who wanted to sell me my new Seat. 
+                                After negotiating hard on the basis of the real sales price in the report, 
+                                we were able to negotiate €250. "
+                            </h6>
+                        </div>
                     </div>
-                    <div>
-                        <h3>NIENKE</h3>
-                        <h4>" Receive a clear and comprehensive report. 
-                            It has all the information I wanted to know before selling my car. Top! "
-                        </h4>
+                    <div className="carouselItem">
+                        <div className="item">
+                            <h5>NIENKE</h5>
+                            <h6>" Receive a clear and comprehensive report. 
+                                It has all the information I wanted to know before selling my car. Top! "
+                            </h6>
+                        </div>
                     </div>
-                    <div>
-                        <h3>KAJI</h3>
-                        <h4>" Finally a transparent report "</h4>
+                    <div className="carouselItem">
+                        <div className="item">
+                            <h5>KAJI</h5>
+                            <h6>" Finally a transparent report "</h6>
+                        </div>
                     </div>
                 </Slider>
             </div>
--- Front/src/Components/Footer.js
+++ Front/src/Components/Footer.js
@@ -1,9 +1,25 @@
 import react from "react";
-
+import logoImage from '../assets/logo/logo_white.svg';
 const Footer = () => {
     return(
         <>
-            This is Footer
+            <div className="footer d-flex justify-content-center align-items-center">
+                <div>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <img src={logoImage} width={140} height={100}></img>
+                    </div>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <p className="text-center">
+                            Contact  |  Privacy disclaimer  |  Terms and Conditions  |  Have the car appraised
+                        </p>
+                    </div>
+                    <div className="d-flex justify-content-center align-items-center">
+                        <p className="text-center">
+                            © 2022 | KENTEKEN inzicht
+                        </p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
