647d597fd44f0c5917c32ddf890f9c7ad5d80ce9
promisingdev72 <promising.dev72@gmail.com>
2022-03-22 13:50:53 +0800

    update license

--- Front/src/Pages/License.js
+++ Front/src/Pages/License.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import '../assets/css/style.css';
+import confirmImage from '../assets/images/confirm.png';
 
 
 const License = () =>{
     return(
         <>
-            <div className='licenseTitle d-flex justify-content-center align-items-center'>
-                <p className='position-relative'>Request license plate report</p>
-            </div>
-            <div className='reportBoxWrap d-flex justify-content-center align-items-center'>
-                <div className='reportBox'></div>
+            <div className='licenseWrap'>
+                <div className='licenseTitle d-flex justify-content-center align-items-center'>
+                    <p className='position-relative'>Request license plate report</p>
+                </div>
+                <div className='reportBoxWrap d-flex justify-content-center align-items-center'>
+                    <div className='reportBox'>
+                        <p>The sample report is on its way to your mailbox!</p>
+                        <div className='row'>
+                            <div className='col-md-8 col-sm-12'>
+                                <div className='reportLicenseText'>
+                                    Thank you for requesting the sample report! <br></br>
+                                    The sample report is on its way to your mailbox!
+                                </div>
+                                <div className='licenseBtnWrap'>
+                                    <span className='licenseBtn position-relative'>Get my license plate insight</span>
+                                </div>
+                            </div>
+                            <div className='col-md-4 col-sm-12'>
+                                <div className='confirmImage'>
+                                    <img src={confirmImage} width="50%"></img>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
